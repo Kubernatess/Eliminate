359621fdeb9dced94ee73149a4f0122fae896a1c
MyCloudStack <865096492@qq.com>
2020-02-05 23:19:31 +0800

    sc

---
+++ src/data/LinkLogic.ts
@@ -0,0 +1,239 @@
+/**
+ * 地图相关的算法
+ */
+class LinkLogic {
+	// 用于存放可消除的元素id
+	public static lines:number[][];
+
+	// 交换卡片元素后,检查是否有可消除的一行或一列元素
+	public static isHaveLine():boolean {
+		LinkLogic.lines = [];
+		// 记录当前类型的一个指针
+		let currentType:string = "";
+		// 当前检索类型的数量
+		let typeNum:number = 0;
+		// 横向循环判断
+		for(let i=0;i<GameData.MaxRow;i++){
+			for(let j=0;j<GameData.MaxColumn;j++){
+				// !=-1表示地图块可用,地图块上可能有卡片元素,但也可能没有卡片元素
+				if(GameData.mapData[i][j] != -1){
+					// 如果当前的类型与对象池存放的卡片元素类型不同,表明上一组的检测结束了
+					if(currentType != GameData.elements[GameData.mapData[i][j]].type){
+						// 如果上一次的检索类型数量大于等于3,把所有可消除元素全部存起来
+						if(typeNum >= 3){
+							let arr:number[] = [];
+							for(let k=0;k<typeNum;k++){
+								arr.push(GameData.mapData[i][j-k-1]]);
+							}
+							LinkLogic.lines.push(arr);
+						}
+						// 重置当前类型和检索类型计数器
+						currentType = GameData.elements[GameData.mapData[i][j]].type;
+						typeNum = 1;
+					}
+					else {
+						typeNum++;
+					}
+				}
+				// 否则mapData[i][j]==-1,表示地图块不可使用,相当于把数据截断了
+				else {
+					if(typeNum >= 3){
+						let arr:number[] = [];
+						for(let k=0;k<typeNum;k++){
+							arr.push(GameData.mapData[i][j-k-1]);
+						}
+						LinkLogic.lines.push(arr);
+					}
+					// 把当前类型和检索类型计数器纸为空
+					currentType = "";
+					typeNum = 0;
+				}
+			}
+			// 行结尾判断
+			if(typeNum >= 3){
+				let arr:number[] = [];
+				for(let k=0;k<typeNum;k++){
+					arr.push(GameData.mapData[i][j-k-1]]);
+				}
+				LinkLogic.lines.push(arr);
+			}
+			// 把当前类型和检索类型计数器纸为空
+			currentType = "";
+			typeNum = 0;
+		}
+		// 纵向循环判断,方法同上
+		for(let i=0;i<GameData.MaxRow;i++){
+			for(let j=0;j<GameData.MaxColumn;j++){
+				if(GameData.mapData[j][i] != -1){
+					if(currentType != GameData.elements[GameData.mapData[j][i]].type){
+						if(typeNum >= 3){
+							let arr:number[] = [];
+							for(let k=0;k<typeNum;k++){
+								arr.push(GameData.mapData[j-k-1][i]);
+							}
+							LinkLogic.lines.push(arr);
+						}
+						currentType = GameData.elements[GameData.mapData[j][i]].type;
+						typeNum = 1;
+					}
+					else {
+						typeNum++;
+					}
+				}
+				else {
+					if(typeNum >= 3){
+					let arr:number[] = [];
+					for(let k=0;k<typeNum;k++){
+						arr.push(GameData.mapData[j-k-1][i]);
+					}
+					LinkLogic.lines.push(arr);
+				}
+				currentType = "";
+				typeNum = 0;
+				}
+			}
+			// 列末尾判断
+			if(typeNum >= 3){
+				let arr:number[] = [];
+				for(let k=0;k<typeNum;k++){
+					arr.push(GameData.mapData[j-k-1][i]);
+				}
+				LinkLogic.lines.push(arr);
+			}
+			currentType = "";
+			typeNum = 0;
+		}
+		// 返回最终结果
+		if(LinkLogic.lines.length != 0){
+			return true;
+		}
+		return false;
+	}
+
+	// 全局地预检索可消除元素,如果有可消除元素,则继续游戏,如果没有,则打乱所有元素的顺序
+	public static isNextHaveLine():boolean {
+		for(let i=0;i<GameData.MaxRow;i++){
+			for(let j=0;j<GameData.MaxColumn;j++){
+				// mapData[i][j]表示当前地图块,前提条件是当前这地图块必须可用
+				if(GameData.mapData[i][j]!=-1){
+					// 先考虑横向的情况,先考虑不在极限值的情况下,找到两个相同类型的相邻排列的卡片元素
+					if(j<(GameData.MaxColumn-1) && GameData.mapData[i][j+1]!=-1 && GameData.elements[GameData.mapData[i][j]].type==GameData.elements[GameData.mapData[i][j+1]].type){
+						// 寻找周围六个元素,同样先保证地图块可用,先寻找左侧三个元素
+						if(j>0 && GameData.mapData[i][j-1]!=-1){
+							// 判断左上角,保证相邻的这两个元素不越界
+							if(i>0 && j>0 && GameData.mapData[i-1][j-1] && GameData.mapData[i-1][j-1]!=-1 && GameData.elements[GameData.mapData[i-1][j-1]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+							// 判断左下角
+							if(i<(GameData.MaxRow-1) && j>0 && GameData.mapData[i+1][j-1] && GameData.mapData[i+1][j-1]!=-1 && GameData.elements[GameData.mapData[i+1][j-1]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+							// 判断左侧跳格
+							if(j>1 && GameData.mapData[i][j-2] && GameData.mapData[i][j-2]!=-1 && GameData.elements[GameData.mapData[i][j-2]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+						}
+						// 寻找右侧三个元素,以当前地图块为准,以相隔右侧一个地图块作为中心点,去寻找右侧三个元素
+						if(j<(GameData.MaxColumn-1) && GameData.mapData[i][j+2]!=-1){
+							// 判断右上角
+							if(j<(GameData.MaxColumn-2) && i>0 && GameData.mapData[i-1][j+2] && GameData.mapData[i-1][j+2]!=-1 && GameData.elements[GameData.mapData[i-1][j+2]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+							// 判断右下角
+							if(j<(GameData.MaxColumn-2) && i<(GameData.MaxRow-1) && GameData.mapData[i+1][j+2] && GameData.mapData[i+1][j+2]!=-1 && GameData.elements[GameData.mapData[i+1][j+2]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+							// 判断右侧跳格
+							if(j<(GameData.MaxColumn-3) && GameData.mapData[i][j+3] && GameData.mapData[i][j+3]!=-1 && GameData.elements[GameData.mapData[i][j+3]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+						}
+					}
+					// 考虑纵向的情况,同样也是不考虑极限值的情况,找到两个相同类型的相邻纵向排列的卡片元素
+					if(i<(GameData.MaxRow-1) && GameData.mapData[i+1][j]!=-1 && GameData.elements[GameData.mapData[i][j]].type==GameData.elements[GameData.mapData[i+1][j]].type){
+						// 寻找周围六个元素,同样先保证地图块可用,先寻找上侧三个元素
+						if(i>0 && GameData.mapData[i-1][j]!=-1){
+							// 判断左上角
+							if(i>0 && j>0 && GameData.mapData[i-1][j-1] && GameData.mapData[i-1][j-1]!=-1 && GameData.elements[GameData.mapData[i-1][j-1]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+							// 判断右上角
+							if(j<(GameData.MaxColumn-1) && i>0 && GameData.mapData[i-1][j+1] && GameData.mapData[i-1][j+1]!=-1 && GameData.elements[GameData.mapData[i-1][j+1]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+							// 判断上方跳格
+							if(i>1 && GameData.mapData[i-2][j] && GameData.mapData[i-2][j]!=-1 && GameData.elements[GameData.mapData[i-2][j]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+						}
+						// 寻找下方三个元素,以当前地图块为准,以相隔下方一个地图块作为中心点,去寻找下方三个元素
+						if(i<(GameData.MaxRow-2) && GameData.mapData[i+2][j]!=-1){
+							// 判断左下角
+							if(j>0 && GameData.mapData[i+2][j-1] && GameData.mapData[i+2][j-1]!=-1 && GameData.elements[GameData.mapData[i+2][j-1]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+							// 判断右下角
+							if(j<(GameData.MaxColumn-2) && GameData.mapData[i+2][j+1] && GameData.mapData[i+2][j+1]!=-1 && GameData.elements[GameData.mapData[i+2][j+1]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+							// 判断下方跳格
+							if(i<(GameData.MaxRow-3) && GameData.mapData[i+3][j] && GameData.mapData[i+3][j]!=-1 && GameData.elements[GameData.mapData[i+3][j]].type==GameData.elements[GameData.mapData[i][j]].type){
+								return true;
+							}
+						}
+					}
+					// 方式2横向情况
+					if(j<(GameData.MaxColumn-2) && GameData.mapData[i][j+2]!=-1 && GameData.elements[GameData.mapData[i][j]].type==GameData.elements[GameData.mapData[i][j+2]].type){
+						if(GameData.mapData[i][j+1]!=-1){
+							if(i>0 && GameData.mapData[i-1][j+1] && GameData.mapData[i-1][j+1]!=-1 && GameData.elements[GameData.mapData[i][j]].type==GameData.elements[GameData.mapData[i-1][j+1]].type){
+								return true;
+							}
+							if(i<(GameData.MaxRow-1) && GameData.mapData[i+1][j+1] && GameData.mapData[i+1][j+1]!=-1 && GameData.elements[GameData.mapData[i][j]].type==GameData.elements[GameData.mapData[i+1][j+1]].type){
+								return true;
+							}
+						}
+					}
+					// 方式2纵向情况
+					if(i<(GameData.MaxRow-2) && GameData.mapData[i+2][j]!=-1 && GameData.elements[GameData.mapData[i][j]].type==GameData.elements[GameData.mapData[i+2][j]].type){
+						if(GameData.mapData[i+1][j]!=-1){
+							if(j<(GameData.MaxColumn-1) && GameData.mapData[i+1][j+1] && GameData.mapData[i+1][j+1]!=-1 && GameData.elements[GameData.mapData[i][j]].type==GameData.elements[GameData.mapData[i+1][j+1]].type){
+								return true;
+							}
+							if(i<(GameData.MaxRow-1) && j>0 && GameData.mapData[i+1][j-1] && GameData.mapData[i+1][j-1]!=-1 && GameData.elements[GameData.mapData[i][j]].type==GameData.elements[GameData.mapData[i+1][j-1]].type){
+								return true;
+							}
+						}
+					}
+				}	
+			}
+		}
+		return false;
+	}
+
+	// 通过互换元素之后,是否可以消除,p1、p2指的是交换的两个元素的location
+	public static isHaveLineByIndex(p1:number , p2:number):boolean {
+		// 定义两个暂存变量
+		let p1n:number = p1;
+		let p2n:number = p2;
+		// 获取两个卡片元素的id
+		let p1id:number = GameData.mapData[Math.floor(p1/GameData.MaxColumn)][p1%GameData.MaxRow];
+		let p2id:number = GameData.mapData[Math.floor(p2/GameData.MaxColumn)][p2%GameData.MaxRow];
+		// 交换两个卡片元素的id
+		GameData.mapData[Math.floor(p1/GameData.MaxColumn)][p1%GameData.MaxRow] = p2id;
+		GameData.mapData[Math.floor(p2/GameData.MaxColumn)][p2%GameData.MaxRow] = p1id;
+		// 判断交换之后是否可以消除元素
+		let rel:boolean = LinkLogic.isHaveLine();
+		if(rel){
+			// 如果交换元素后可以连起来,则p1、p2交换成功,并交换location属性
+			GameData.elements[p1id].location = p2;
+			GameData.elements[p2id].location = p1;
+			return true;
+		}
+		else {
+			// 否则把p1id和p2id互换回来
+			GameData.mapData[Math.floor(p1/GameData.MaxColumn)][p1%GameData.MaxRow] = p1id;
+			GameData.mapData[Math.floor(p2/GameData.MaxColumn)][p2%GameData.MaxRow] = p2id;
+		}
+		return false;
+	}
+}
---
+++ src/view/ElementView.ts
@@ -0,0 +1,200 @@
+class ElementView extends egret.Sprite {
+	private thisparent:egret.Sprite;
+	// 游戏中的元素
+	public constructor(tparent:egret.Sprite) {
+		super();
+		this.thisparent = tparent;
+		this.init();
+	}
+
+	// 位置编号,用于位置移动
+	public location:number = 0;
+
+	// 与GameData.elements[]数组下标相同
+	public _id:number = -1;
+	public get id():number {
+		return this._id;
+	}
+	public set id(val:number) {
+		this._id = val;
+	}
+
+	// 当前元素中的位图数据
+	private bitmap:egret.Bitmap;
+
+	// 初始化所有数据
+	private init(){
+		this.touchEnabled = true;
+		this.touchChildren = false;
+		this.bitmap = new egret.Bitmap();
+		let bitwidth:number = (GameData.stageW-40)/GameData.MaxColumn;
+		this.bitmap.width = bitwidth-10;
+		this.bitmap.height = bitwidth-10;
+		this.bitmap.x = -1*bitwidth/2;
+		this.bitmap.y = -1*bitwidth/2;
+		this.addChild(this.bitmap);
+	}
+
+	// 设置贴图
+	public setTexture(val:string){
+		this.bitmap.texture = RES.getRes(val);
+	}
+
+	private _focus:boolean = false;
+	public get focus():boolean{
+		return this._focus;
+	}
+	private _focusMc:egret.MovieClip;
+	// 设置选中状态的焦点样式
+	public setFocus(val:boolean){
+		if(val!=this._focus){
+			this._focus = val;
+			if(!this._focusMc){
+				let tex = RES.getRes("focusmc_png");
+				let data = RES.getRes("focusmc_json");
+				let mcf:egret.MovieClipDataFactory = new egret.MovieClipDataFactory(tex , data);
+				this._focusMc = new egret.MovieClip(mcf.generateMovieClipData("focusmc"));
+				this._focusMc.x = this._focusMc.width/-2;
+				this._focusMc.y = this._focusMc.height/-2;
+				this._focusMc.width = this.bitmap.width;
+				this._focusMc.height = this.bitmap.height;
+			}
+			if(val){
+				this.addChild(this._focusMc);
+				this._focusMc.play(-1);
+			}
+			else{
+				if(this._focusMc.parent){
+					this._focusMc.stop();
+					this.removeChild(this._focusMc);
+				}
+			}
+		}
+	}
+
+	public speed:number = 700;
+	// 移动到新位置,使用cubicInOut算法移动,直线运动
+	public move(){
+		let tw:egret.Tween = egret.Tween.get(this);
+		tw.to({x:this.targetX(),y:this.targetY()} , this.speed , egret.Ease.cubicInOut);
+	}
+
+	public show(wait:number){
+		let tw:egret.Tween = egret.Tween.get(this);
+		tw.wait(wait , false);
+		tw.call(this.addThisToParent,this);
+		tw.to({x:this.targetX(),y:this.targetY()} , this.speed , egret.Ease.bounceOut);
+	}
+
+	// 添加到父级显示对象
+	private addThisToParent(){
+		if(!this.parent){
+			this.thisparent.addChild(this);
+		}
+	}
+
+	public targetX():number{
+		let girdwidth:number = (GameData.stageW-40)/GameData.MaxColumn;
+		let xx:number = 20+girdwidth*(this.location%GameData.MaxColumn)+girdwidth/2+5;
+		return xx;
+	}
+
+	public targetY():number {
+		let girdwidth:number = (GameData.stageW-40)/GameData.MaxColumn;
+		let startY:number = (GameData.stageH-(GameData.stageW-30)/6-60)-girdwidth*GameData.MaxColumn;
+		let yy:number = startY+girdwidth*(Math.floor(this.location/GameData.MaxColumn))+girdwidth/2+5;
+		return yy;
+	}
+
+	// 移动到另一个位置,然后再移动回来
+	public moveAndBack(location:number , isscale:boolean=false){
+		let girdwidth:number = (GameData.stageW-40)/GameData.MaxColumn;
+		let xx:number = 20+girdwidth*(location%GameData.MaxColumn)+girdwidth/2+5;
+		let startY:number = (GameData.stageH-(GameData.stageW-30)/6-60)-girdwidth*GameData.MaxColumn;
+		let yy:number = startY+girdwidth*(Math.floor(location/GameData.MaxColumn))+girdwidth/2+5;
+		// 移动的时候,不仅会移动位置,还会放大或者缩小,移动回来时,scale都设置为1
+		let tw:egret.Tween = egret.Tween.get(this);
+		if(isscale){
+			tw.to({x:xx,y:yy,scaleX:1.2,scaleY:1.2},300,egret.Ease.cubicInOut).call(this.back,this);
+		}
+		else{
+			tw.to({x:xx,y:yy,scaleX:0.8,scaleY:0.8},300,egret.Ease.cubicInOut).call(this.back,this);
+		}
+	}
+
+	private back(){
+		let tw:egret.Tween = egret.Tween.get(this);
+		tw.to({x:this.targetX(),y:this.targetY(),scaleX:1,scaleY:1},300,egret.Ease.cubicInOut);
+	}
+
+	// 移动到另一个位置
+	public moveAndScale(location:number , isscale:boolean=false){
+		let girdwidth:number = (GameData.stageW-40)/GameData.MaxColumn;
+		let xx:number = 20+girdwidth*(location%GameData.MaxColumn)+girdwidth/2+5;
+		let startY:number = (GameData.stageH-(GameData.stageW-30)/6-60)-girdwidth*GameData.MaxColumn;
+		let yy:number = startY+girdwidth*(Math.floor(location/GameData.MaxColumn))+girdwidth/2+5;
+
+		let tw:egret.Tween = egret.Tween.get(this);
+		if(isscale){
+			tw.to({x:xx,y:yy,scaleX:1.4,scaleY:1.4},300,egret.Ease.cubicInOut).call(this.backScale,this);
+		}
+		else{
+			tw.to({x:xx,y:yy,scaleX:0.6,scaleY:0.6},300,egret.Ease.cubicInOut).call(this.backScale,this);
+		}
+	}
+
+	private backScale(){
+		let tw:egret.Tween = egret.Tween.get(this);
+		tw.to({scaleX:1,scaleY:1},300,egret.Ease.backOut).call(this.canRemove,this);
+	}
+
+	private canRemove(){
+		let evt:ElementViewManageEvent = new ElementViewManageEvent(ElementViewManageEvent.REMOVE_ANIMATION_OVER);
+		this.dispatchEvent(evt);
+	}
+
+	// 播放曲线动画
+	public playCurveMove(tx:number , ty:number){
+		let tw:egret.Tween = egret.Tween.get(this);
+		tw.to({x:tx,y:ty},700,egret.Ease.quadOut).call(this.overCurveMove , this);
+	}
+	private overCurveMove(){
+		if(this.parent){
+			this.parent.removeChild(this);
+		}
+		let evt:ElementViewManageEvent = new ElementViewManageEvent(ElementViewManageEvent.UPDATE_MAP);
+		this.dispatchEvent(evt);
+	}
+
+	// 播放直接消除动画,自己放大,然后缩回到原有大小,然后删除
+	public playRemoveAni(){
+		let tw:egret.Tween = egret.Tween.get(this);
+		tw.to({scaleX:1.4,scaleY:1.4},300,egret.Ease.cubicInOut).to({scaleX:0.1,scaleY:0.1},300,egret.Ease.cubicInOut).call(this.removeAniCall,this);
+	}
+	private removeAniCall(){
+		if(this.parent){
+			this.parent.removeChild(this);
+		}
+		let evt:ElementViewManageEvent = new ElementViewManageEvent(ElementViewManageEvent.UPDATE_MAP);
+		this.dispatchEvent(evt);
+	}
+
+	// 根据列编号,重新计算元素X轴位置,从其Y轴开始播放下落动画
+	public moveNewLocation(){
+		if(!this.parent){
+			let girdwidth:number = (GameData.stageW-40)/GameData.MaxColumn;
+			let startY:number = (GameData.stageH-(GameData.stageW-30)/6-60)-girdwidth*GameData.MaxColumn;
+			this.y = startY-this.width;
+			this.scaleX = 1;
+			this.scaleY = 1;
+			this.x = this.targetX();
+			this.thisparent.addChild(this);
+		}
+		let tw:egret.Tween = egret.Tween.get(this);
+		tw.to({x:this.targetX(),y:this.targetY()},this.speed,egret.Ease.bounceOut).call(this.moveNewLocationOver , this);
+	}
+	private moveNewLocationOver(){
+		let evt:ElementViewManageEvent = new ElementViewManageEvent(ElementViewManageEvent.UPDATE_VIEW_OVER);
+		this.dispatchEvent(evt);
+	}
+}
